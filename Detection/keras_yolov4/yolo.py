ed2f0cb0fbabef2193dcfe7a233c9f32b9de0686
sh-zh-7 <2431297348@qq.com>
2020-05-25 22:23:14 +0800

    Add detection part

---
+++ Detection/keras_yolov4/yolo.py
@@ -0,0 +1,127 @@
+import colorsys
+import os
+
+import numpy as np
+from PIL import Image
+from keras import backend as K
+from keras.layers import Input
+
+from Detection.keras_yolov4.yolo4.model import yolo_eval, yolo4_body
+from Detection.keras_yolov4.yolo4.utils import letterbox_image
+
+score = 0.5
+iou = 0.5
+
+model_path = 'Detection/keras_yolov4/model_data/yolo4_weight.h5'
+anchors_path = 'Detection/keras_yolov4/model_data/yolo4_anchors.txt'
+classes_path = 'Detection/keras_yolov4/model_data/coco_classes.txt'
+
+class Yolo4:
+    def get_class(self):
+        classes_path = os.path.expanduser(self.classes_path)
+        with open(classes_path) as f:
+            class_names = f.readlines()
+        class_names = [c.strip() for c in class_names]
+        return class_names
+
+    def get_anchors(self):
+        anchors_path = os.path.expanduser(self.anchors_path)
+        with open(anchors_path) as f:
+            anchors = f.readline()
+        anchors = [float(x) for x in anchors.split(',')]
+        return np.array(anchors).reshape(-1, 2)
+
+    def load_yolo(self):
+        model_path = os.path.expanduser(self.model_path)
+        assert model_path.endswith('.h5'), 'Keras model or weights must be a .h5 file.'
+
+        self.class_names = self.get_class()
+        self.anchors = self.get_anchors()
+
+        num_anchors = len(self.anchors)
+        num_classes = len(self.class_names)
+
+        # Generate colors for drawing bounding boxes.
+        hsv_tuples = [(x / len(self.class_names), 1., 1.)
+                      for x in range(len(self.class_names))]
+        self.colors = list(map(lambda x: colorsys.hsv_to_rgb(*x), hsv_tuples))
+        self.colors = list(
+            map(lambda x: (int(x[0] * 255), int(x[1] * 255), int(x[2] * 255)),
+                self.colors))
+
+        self.sess = K.get_session()
+
+        # Load model, or construct model and load weights.
+        self.yolo4_model = yolo4_body(Input(shape=(608, 608, 3)), num_anchors//3, num_classes)
+        self.yolo4_model.load_weights(model_path)
+
+        print('{} model, anchors, and classes loaded.'.format(model_path))
+
+        if self.gpu_num>=2:
+            self.yolo4_model = multi_gpu_model(self.yolo4_model, gpus=self.gpu_num)
+
+        self.input_image_shape = K.placeholder(shape=(2, ))
+        self.boxes, self.scores, self.classes = yolo_eval(self.yolo4_model.output, self.anchors,
+                len(self.class_names), self.input_image_shape,
+                score_threshold=self.score)
+
+    def __init__(self, target_class, gpu_num=1):
+        self.score = score
+        self.iou = iou
+        self.target_class =target_class
+        self.anchors_path = anchors_path
+        self.classes_path = classes_path
+        self.model_path = model_path
+        self.gpu_num = gpu_num
+        self.load_yolo()
+
+    def close_session(self):
+        self.sess.close()
+
+    def detect_image(self, image, model_image_size=(608, 608)):
+        return_boxs = []
+        return_class_names = []
+
+        boxed_image = letterbox_image(image, tuple(reversed(model_image_size)))
+        image_data = np.array(boxed_image, dtype='float32')
+
+        image_data /= 255.
+        image_data = np.expand_dims(image_data, 0)  # Add batch dimension.
+
+        out_boxes, out_scores, out_classes = self.sess.run(
+            [self.boxes, self.scores, self.classes],
+            feed_dict={
+                self.yolo4_model.input: image_data,
+                self.input_image_shape: [image.size[1], image.size[0]],
+                K.learning_phase(): 0
+            })
+
+        for i, c in reversed(list(enumerate(out_classes))):
+            predicted_class = self.class_names[c]
+            if predicted_class not in self.target_class:
+                continue
+            box = out_boxes[i]
+            score = out_scores[i]
+
+            return_boxs.append(tuple(box))
+            return_class_names.append(predicted_class)
+
+        return return_boxs, return_class_names
+
+if __name__ == '__main__':
+    model_image_size = (608, 608)
+
+    yolo4_model = Yolo4(["traffic light"])
+
+    while True:
+        img = input('Input image filename:')
+        try:
+            image = Image.open(img)
+        except:
+            print('Open Error! Try again!')
+            continue
+        else:
+            result = yolo4_model.detect_image(image, model_image_size=model_image_size)
+            print(result)
+
+    yolo4_model.close_session()
---
+++ Detection/traffic_light.py
@@ -0,0 +1,83 @@
+import cv2
+import numpy as np
+from Detection.keras_yolov4.yolo import Yolo4
+import Tracking.video as video
+from PIL import Image
+import matplotlib.pyplot as plt
+
+def get_white_pixel_count(img):
+    """
+    Image with the most white pixels is corresponding color
+    :param img: target image
+    :return: white pixels count
+    """
+    count = 0
+    for line in img:
+        for element in line:
+            if element != 0:
+                count += 1
+    return count
+
+def get_traffic_light_color(img):
+    """
+    :param img: traffic light roi
+    :return: "green", "red" or "yellow"
+    """
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+    # Judge green
+    green_lower = np.array([35, 43, 46])
+    green_upper = np.array([77, 255, 255])
+    green_mask = cv2.inRange(hsv, lowerb=green_lower, upperb=green_upper)
+    cyan_lower = np.array([78, 43, 46])
+    cyan_upper = np.array([99, 255, 255])
+    cyan_mask = cv2.inRange(hsv, lowerb=cyan_lower, upperb=cyan_upper)
+    green_count = get_white_pixel_count(green_mask + cyan_mask)
+
+    # Judge red
+    red1_lower = np.array([0, 43, 46])
+    red1_upper = np.array([10, 255, 255])
+    red1_mask = cv2.inRange(hsv, lowerb=red1_lower, upperb=red1_upper)
+    red2_lower = np.array([156, 43, 46])
+    red2_upper = np.array([180, 255, 255])
+    red2_mask = cv2.inRange(hsv, lowerb=red2_lower, upperb=red2_upper)
+    red_count = get_white_pixel_count(red1_mask + red2_mask)
+
+    # Judge yellow
+    orange_lower = np.array([11, 43, 46])
+    orange_upper = np.array([25, 255, 255])
+    orange_mask = cv2.inRange(hsv, lowerb=orange_lower, upperb=orange_upper)
+    yellow_lower = np.array([26, 43, 46])
+    yellow_upper = np.array([34, 255, 255])
+    yellow_mask = cv2.inRange(hsv, lowerb=yellow_lower, upperb=yellow_upper)
+    yellow_count = get_white_pixel_count(orange_mask + yellow_mask)
+
+    # Decide colors
+    ret_color, max_count = None, 0
+    colors, counts = ("green", "red", "yellow"), (green_count, red_count, yellow_count)
+    for color, count in zip(colors, counts):
+        if count > max_count:
+            ret_color = color
+            max_count = count
+    return ret_color
+
+
+if __name__ == '__main__':
+    dataloader = video.Video("../Videos/video-01.avi")
+    # Image with static background
+    img = dataloader.get_one_frame()
+    img = Image.fromarray(img[..., ::-1])
+
+    # Start detection traffic light
+    yolo = Yolo4(["traffic light"])
+    bboxes, _ = yolo.detect_image(img)
+
+    for bbox in bboxes:
+        img = np.array(img)     # Convert to numpy object
+        x1, x2, y1, y2 = int(bbox[0]), int(bbox[2]), int(bbox[1]), int(bbox[3])
+        roi = img[x1:x2, y1:y2, :]
+        plt.imshow(roi)
+        plt.show()
+        print(get_traffic_light_color(roi))
+
+
