33b08347fa26da7a60b3a25d19eb8e45edc54508
sh-zh-7 <2431297348@qq.com>
2020-05-27 01:21:30 +0800

    Add lpr part

--- LPR/lpr.py
+++ LPR/lpr.py
@@ -1,5 +1,21 @@
-
+import cv2
+from hyperlpr import *
 
 def license_plate_recognize(car_img):
-    # Add you codes Here
-    pass
+    """
+    :param car_img
+    :return: License plate and its confidence
+    """
+    try:
+        result = HyperLPR_plate_recognition(car_img)
+        return result[0][0], result[0][1]
+    except:
+        # Can't recognize
+        # Include bbox's size is too small(0)
+        # And index error
+        return "Can't recognize", 0
+
+
+if __name__ == '__main__':
+    src = cv2.imread("test.jpg")
+    license_plate_recognize(src)
