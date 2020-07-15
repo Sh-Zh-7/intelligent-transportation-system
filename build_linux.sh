b678686914e2224c6a5b1d51c4a3c46fc37cd141
sh-zh-7 <2431297348@qq.com>
2020-07-15 13:12:08 +0800

    Add build script

--- build_linux.sh
+++ build_linux.sh
@@ -1,6 +1,9 @@
-# conda update -n base conda	# Upgrade conda
-conda install x264=='1!152.20180717' ffmpeg=4.0.2 -c conda-forge	# Download ffmpeg and x264encoder
-python -m pip install --upgrade pip
+#! /bin/sh
+
+echo "Start building..."
+# Download ffmpeg and x264encoder
+conda install x264=='1!152.20180717' ffmpeg=4.0.2 -c conda-forge 
 pip3 install -r requirements.txt
-# conda install --yes --file requirements.txt
+export PYTHONPATH="${PYTHONPATH}:./Model/"
+echo "Building ends..."
 
